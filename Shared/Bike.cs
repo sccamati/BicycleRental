604cc5a0efa362c90b76a8d3e2030c1498084dde
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-03-17 19:26:45 +0100

    add models

---
+++ Shared/Bike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleRental.Shared
+{
+    public class Bike
+    {
+        public int Id { get; set; }
+        public string SerialNumber { get; set; }
+        public Type Type { get; set; }
+        public bool IsBorrowed { get; set; }
+        public int ProductionDate { get; set; }
+    }
+}
---
+++ Shared/Rental.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleRental.Shared
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Bike Bike { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
