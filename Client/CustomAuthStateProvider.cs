ce5678fe4b0bb529d17ca2500b7628f244722e15
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-03-20 17:12:46 +0100

    client authentication

---
+++ Client/CustomAuthStateProvider.cs
@@ -0,0 +1,35 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BicycleRental.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            string email = await _localStorage.GetItemAsStringAsync("email");
+            if (!string.IsNullOrEmpty(email))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, email),
+                }, "test authentication type");
+
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+           
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,7 @@
+global using BicycleRental.Shared;
 using BicycleRental.Client;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +10,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
