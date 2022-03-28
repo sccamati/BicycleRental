f7f90befe8a0539df7719b8a8315c7d47f458f85
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-03-28 10:40:01 +0200

    add create and update user

--- Server/Controllers/UserController.cs
+++ Server/Controllers/UserController.cs
@@ -1,5 +1,5 @@
 ﻿using BicycleRental.Server.Services.Interfaces;
-using BicycleRental.Shared.Responses;
+using BicycleRental.Shared.Requests.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BicycleRental.Server.Controllers
@@ -9,17 +9,49 @@ namespace BicycleRental.Server.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-        private readonly DataContext _dataContext;
-        public UserController(IUserService userService, DataContext dataContext)
+        public UserController(IUserService userService)
         {
             _userService = userService;
-            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] UserRequest user)
+        {
+            User newUser = new User
+            {
+                Email = user.Email,
+                Password = user.Password,
+                Role = user.Role
+            };
+
+            await _userService.Add(newUser);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async void UpdateUser([FromBody] UpdateUserRequest user)
+        {
+            User userToUpdate = new User
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Password = user.Password,
+                Role = user.Role
+            };
+            await _userService.Update(userToUpdate);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUserById([FromQuery] int id)
         {
             User user = await _userService.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
             return Ok(user);
         }
 
@@ -27,7 +59,20 @@ namespace BicycleRental.Server.Controllers
         public async Task<ActionResult<List<User>>> GetUsers()
         {
             var users = await _userService.GetAllWithRoles();
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
             return Ok(users);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            await _userService.DeleteById(id);
+            return Ok();
         }
     }
 }
