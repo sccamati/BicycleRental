87950552c34e5de3d4017825e28f8b7f87ea4d79
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-03-28 11:01:46 +0200

    change user request

---
+++ Shared/Requests/User/UserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleRental.Shared.Requests.User
+{
+    public class UserRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
