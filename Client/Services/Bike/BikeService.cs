756ed6f6012f4998e40bff9f49b7246803b37333
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-04-13 21:12:14 +0200

    add bikeService in client

---
+++ Client/Services/Bike/BikeService.cs
@@ -0,0 +1,12 @@
+﻿using BicycleRental.Shared.Dto;
+using Blazored.LocalStorage;
+
+namespace BicycleRental.Client.Services.Bike
+{
+    public class BikeService : Service<BikeDto>, IBikeService
+    {
+        public BikeService(HttpClient httpClient, ILocalStorageService localStorageService) : base(httpClient, localStorageService)
+        {
+        }
+    }
+}
---
+++ Client/Services/Bike/IBikeService.cs
@@ -0,0 +1,8 @@
+﻿using BicycleRental.Shared.Dto;
+
+namespace BicycleRental.Client.Services.Bike
+{
+    public interface IBikeService : IService<BikeDto>
+    {
+    }
+}
