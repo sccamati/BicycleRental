3e264cf9e70f2d29c971961a4bbea173c7fd3051
Mateusz Aleksandrowicz <m.aleksandrowicz@blunovation.com>
2022-05-26 15:36:36 +0200

    change authorization

--- Client/CustomAuthStateProvider.cs
+++ Client/CustomAuthStateProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
+using System.Text.Json;
 
 namespace BicycleRental.Client
 {
@@ -18,18 +19,33 @@ namespace BicycleRental.Client
             string token = await _localStorage.GetItemAsStringAsync("token");
             if (!string.IsNullOrEmpty(token))
             {
-                var identity = new ClaimsIdentity(new[]
-                {
-                    new Claim("token", token),
-                }, "Jwt authentication type");
+                var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "token");
+                var user = new ClaimsPrincipal(identity);
 
-                state = new AuthenticationState(new ClaimsPrincipal(identity));
+                state = new AuthenticationState(new ClaimsPrincipal(user));
             }
 
             NotifyAuthenticationStateChanged(Task.FromResult(state));
 
             return state;
-           
+
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
         }
     }
 }
